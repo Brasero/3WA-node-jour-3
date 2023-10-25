5bdc4ca8ecf36d2edf329f2c51ab78c4f7796b2f
Brasero <brandonricci4@gmail.com>
2023-10-25 16:05:57 +0200

    preparation de l'exercice

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 import {createServer} from 'node:http'
 import {readFileSync} from 'node:fs'
-import {shuffle} from './src/utils.js'
-
-
-const users = [
-    'Alan',
-    'Sophie',
-    'Bernard',
-    'Elie'
-];
 
 const server = createServer(async (req, res) => {
-
+    //Répond à la demande de favicon (icon de l'onglet de page)
     if (req.url === '/favicon.ico') {
         res.writeHead(200, {
             "Content-type": "image/x-icon"
@@ -22,24 +13,29 @@ const server = createServer(async (req, res) => {
 
     if(req.url === '/') {
         let html = readFileSync('__header.html', 'utf8')
-        html += '<ul>'
-        users.forEach((user) => {
-            html += `<li>${user}</li>`
-        })
-        html += '</ul><a href="/shuffle">Shuffle</a>'
-        html += readFileSync('__footer.html', 'utf8')
-        res.end(html)
-    } else if(req.url === '/shuffle') {
-        let html = readFileSync('__header.html', 'utf8')
-        const htmlBody = shuffle(users)
-        html += htmlBody
-        html += readFileSync('__footer.html', 'utf8')
-        res.end(html)
-    } else {
-        res.writeHead(404)
-        res.end()
-    }
 
+        //Contenu >>
+
+        html += readFileSync('__footer.html', 'utf8')
+        res.end(html)
+
+    } else if(req.url === '/shuffle') { // << Todo : Url à modifier
+        let html = readFileSync('__header.html', 'utf8')
+
+        //Contenu >>
+
+        html += readFileSync('__footer.html', 'utf8')
+        res.end(html)
+
+    } else {
+        res.writeHead(404, {
+            "Content-type": 'text/html'
+        })
+        res.end(`
+            <h1>Page introuvable</h1>
+            <a href="/"><button>Go Home</button></a>
+        `)
+    }
 })
 
 server.listen('8888')
