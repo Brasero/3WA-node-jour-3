cbbb5d5d71b1ebaed0d7c9719fb6c909b6790c46
Brasero <brandonricci4@gmail.com>
2023-10-25 14:32:32 +0200

    prepartion de l exercice

--- app.js
+++ app.js
@@ -1,61 +1,16 @@
 import {createServer} from 'node:http'
-import {text, json} from 'node:stream/consumers'
 
 
 const server = createServer(async (req, res) => {
 
-    const url = req.url.replace('/', '')
-
-    const {method} = req
-
-    if(method === 'GET') {
-
-        if (url === 'favicon.ico') {
-            res.writeHead(200, {
-                "Content-type": 'image/ico'
-            })
-            res.end()
-            return
-        }
-
-        const parsedUrl = new URL(req.url, `http://${req.headers.host}`)
-        console.log(parsedUrl)
-
-        const name = parsedUrl.searchParams.get('name')
-        console.log(`name : ${name}`)
-
-
-        let {pathname} = parsedUrl
-        pathname = pathname.substring(1)
-
-        const pathPart = pathname.split('/')
-        console.log(pathPart)
-
-        if (pathPart.length === 1 && pathPart[0] === 'hello') {
-            res.writeHead(200, {
-                "Content-type": 'text/html'
-            })
-            res.write(`
-        <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <title>Mon serveur</title>
-            </head>
-            <body>
-             <h1>Bienvenue sur mon serveur ${name}</h1>
-            </body>
-        </html>
-    `)
-        } else {
-            res.writeHead(404)
-        }
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, {
+            "Content-type": "image/x-icon"
+        });
         res.end()
-    } else if(method === 'POST') {
-        const data = await json(req)
-        console.log(data)
-        res.end()
+        return
     }
+
 })
 
 server.listen('8888')
